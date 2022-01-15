a17d5949db0af741ff29e1f241fd9792e973db3d
Abdulaziz Chalya <abdulazizchalya@gmail.com>
2022-01-15 12:40:53 -0500

    complete CRUD routes

--- src/routers/itemRouter.js
+++ src/routers/itemRouter.js
@@ -2,17 +2,54 @@ const express = require("express");
 const Item = require("../models/item");
 const itemsRouter = express.Router();
 
-
+// get all items
 itemsRouter.get("/", async (req, res) => {
     try {
-        const items = await Item.find();
-        res.status(200).send(items);
+        await Item.find().then((items) => {
+            res.status(200).send(items);
+        });
     }
     catch (error) {
         res.status(500).send(error.message);
     }
 });
 
+// get 1 item by id
+itemsRouter.get("/:id", async (req, res) => {
+    try {
+        const item = await Item.findById(req.params.id);
+        if (item) {
+            res.status(200).send(item);
+        } else {
+            res.status(404).send(`Item with ID ${req.params.id} not found`)
+        }
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
+// update item by id
+itemsRouter.put("/:id", async (req, res) => {
+    try {
+        const itemUpdate = req.body;
+        const id = req.params.id;
+
+        const oldItem = await Item.findById(id);
+
+        if (oldItem) {
+            await Item.findByIdAndUpdate(id, itemUpdate, { new: true, runValidators: true }).then((newItem) => {
+                res.status(200).json(newItem);
+            });
+        } else {
+            res.status(404).send(`Item with ID ${id} not found`);
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+// create item
 itemsRouter.post("/", async (req, res) => {
     try {
         const item = new Item({
@@ -20,7 +57,7 @@ itemsRouter.post("/", async (req, res) => {
             quantity: req.body.quantity,
             price: req.body.price
         });
-        
+
         await item.save().then((newItem) => {
             res.status(201).json(newItem)
         });
@@ -29,5 +66,20 @@ itemsRouter.post("/", async (req, res) => {
         res.status(500).send(error.message);
     }
 });
+
+// delete item by id
+itemsRouter.delete("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        await Item.deleteOne({ _id: id }).then(() => {
+            res.status(204).send();
+        })
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+})
+
 
 module.exports = itemsRouter;
