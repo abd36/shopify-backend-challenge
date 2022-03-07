7a9df35993e31b3c32bafaa16d393df28f0a2aed
Abdulaziz Chalya <abdulazizchalya@gmail.com>
2022-03-07 11:18:40 -0500

    change frontend apiURL to localhost

--- frontend/src/app/item/item.service.ts
+++ frontend/src/app/item/item.service.ts
@@ -12,7 +12,8 @@ import { Item } from './item';
 
 export class ItemService {
 
-  private apiURL: string = "https://guarded-fjord-97076.herokuapp.com/api/items/";
+  // private apiURL: string = "https://guarded-fjord-97076.herokuapp.com/api/items/";
+  private apiURL: string = "localhost:3000/api/items/";
 
   httpOptions = {
     headers: new HttpHeaders({
