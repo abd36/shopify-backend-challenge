bcd965cfcddd6ef2f08b1158dcb194bf9661980f
Abdulaziz Chalya <abdulazizchalya@gmail.com>
2022-03-11 11:21:03 -0500

    display warehouse name and address on homepage

--- frontend/src/app/item/index/index.component.ts
+++ frontend/src/app/item/index/index.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../item.service';
+import { WarehouseService } from 'src/app/warehouse/warehouse.service';
 import { Item } from '../item';
+import { Warehouse } from 'src/app/warehouse/warehouse';
 
 @Component({
   selector: 'app-index',
@@ -11,25 +13,33 @@ export class IndexComponent implements OnInit {
 
   items: Item[] = [];
   deletedItems: Item[] = [];
+  warehouses: { [_id: string]: string[]; } = {};
 
-  constructor(public itemService: ItemService) { }
+  constructor(public itemService: ItemService, public warehouseService: WarehouseService) { }
 
   ngOnInit(): void {
     this.refreshAllItems();
   };
 
   refreshAllItems(): void {
-    this.itemService.getAllItems().subscribe((data: Item[]) => {
-      this.items = data;
-      console.log("items");
-      console.log(this.items);
-    });
-
-    this.itemService.getAllDeletedItems().subscribe((data: Item[]) => {
-      this.deletedItems = data;
-      console.log("deleted items");
-      console.log(this.deletedItems);
-    });
+    this.warehouseService.getAllWarehouses().subscribe((data: Warehouse[]) => {
+      for (const warehouse of data.values()) {
+        this.warehouses[warehouse._id] = [warehouse.name, warehouse.address];
+      }
+      
+      this.itemService.getAllItems().subscribe((data: Item[]) => {
+        this.items = data;
+        console.log("items");
+        console.log(data);
+        console.log(this.items);
+      });
+  
+      this.itemService.getAllDeletedItems().subscribe((data: Item[]) => {
+        this.deletedItems = data;
+        console.log("deleted items");
+        console.log(this.deletedItems);
+      });
+    })
   }
 
   restoreItem(item: Item) {
